b3c709cce8cbcef695f3b503c22f4360ac719040
Emmanuel Fitoussi <manu.fitoussi@gmail.com>
2014-11-08 02:09:30 +0100

    tests of endpoints

--- tests/endpoints.js
+++ tests/endpoints.js
@@ -3,16 +3,33 @@ var Repository = {
 };
 
 module.exports = {
-
   '/example': function (req, res, params, sendSuccess, sendError) {
+    
+    // Response result is: '{"test":"coucou","count":1}'.
+    // HTTP code is 200
     sendSuccess(req, res, {
       test: 'coucou',
-      count: Repository.count++,
-      params: params
+      count: Repository.count++
     });
+
+  },
+
+  '/exampleJSONP': function (req, res, params, sendSuccess, sendError) {
+    
+    // Response result is: myCallback({"test":"coucou","count":1}).
+    // HTTP code is 200
+    sendSuccess(req, res, {
+      test: 'coucou',
+      count: Repository.count++
+    }, params.myCallbackName);
+
   },
 
   '/exampleError': function (req, res, params, sendSuccess, sendError) {
-    sendError(req, res, 401, "erreur d'authentification");
+    
+    // Response result is: '{"code":401,"message":"An error occured durring doing something"}'.
+    // HTTP code is 401
+    sendError(req, res, 401, 'An error occured durring doing something');
+
   }
 };
